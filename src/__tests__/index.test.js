2abab92065cc91011f93f67899ed59a131637ffa
Ellie Packard <epackard@middlebury.edu>
2020-10-18 17:34:20 -0400

    Updated product desc, name, and authors and new test (version 2)

---
+++ src/__tests__/index.test.js
@@ -0,0 +1,16 @@
+/*
+ Tests of our top-level component.
+
+ No tests currently. 
+ */
+
+//import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+
+//import Home from '../pages/index';
+
+
+describe('Top level integration tests', () => {
+  test('First test', () => {
+      return undefined;
+  });
+});
