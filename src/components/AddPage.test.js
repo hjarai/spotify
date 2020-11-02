dcd562fd09703888ebd8eb5d6073834dcd0beced
walkerfrankenberg <65829697+walkerfrankenberg@users.noreply.github.com>
2020-11-02 16:13:32 -0500

    Update AddPage.test.js

--- src/components/AddPage.test.js
+++ src/components/AddPage.test.js
@@ -1,4 +1,4 @@
-import Home from '../pages/index';
+import AddPage from '../components/AddPage.js';
 import { render, screen, fireEvent } from '@testing-library/react';
 
 const song = {title: "Mo Bamba", artist:["Sheck Wes"]};
