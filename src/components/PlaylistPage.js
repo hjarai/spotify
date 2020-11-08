a65e7abfbdbb1b24b66622d194c34ed557cda53f
Arielle Landau <alandau@middlebury.edu>
2020-11-08 14:44:14 -0500

    tests for story 2.1

--- src/components/PlaylistPage.js
+++ src/components/PlaylistPage.js
@@ -1,8 +1,20 @@
 
 import PropTypes from 'prop-types';
+import Head from 'next/head';
+/* eslint-disable no-undef, no-unused-vars */
 
 //displays the Playlist Page, takes one parameter, the OneList to be displayed
 export default function PlaylistPage({ setMode, OneList }) {
+
+    return(
+        //ADD LABELS TO EACH COMPONENT
+        <div> 
+            <Head>
+                <title>Playlist Page Placeholder!</title>
+            </Head>
+        </div>
+
+    );
 
 }
 
--- src/components/PlaylistPage.test.js
+++ src/components/PlaylistPage.test.js
@@ -1,6 +1,6 @@
 import { render, screen, fireEvent } from '@testing-library/react';
-import Playlist from './PlaylistPage';
-
+import PlaylistPage from './PlaylistPage';
+/* eslint-disable no-undef, no-unused-vars */
 const mockOneList = {
     id: 135397,
     title: 'Mo Bamba',
@@ -22,18 +22,39 @@ describe.skip('PlayList Page tests', () => {
         render(<PlaylistPage setMode= {handler} OneList= {mockOneList}/>);
 
         //check if title is rendered
+        const displayTitle = screen.getByLabelText('Title');
+        expect(displayTitle).toBeInTheDocument();
+        expect(displayTitle).toBeVisible();
 
         //check that event id is rendered
+        const displayEventID = screen.getByLabelText('Event ID');
+        expect(displayEventID).toBeInTheDocument();
+        expect(displayEventID).toBeVisible();
 
         //check that description is rendered
+        const displayDescription = screen.getByLabelText('Description');
+        expect(displayDescription).toBeInTheDocument();
+        expect(displayDescription).toBeVisible();
 
         //check that image is rendered
+        const displayImage = screen.getByRole('img'); 
+        expect(displayImage).toBeVisible();
+        expect(displayImage.src).toBe('http://localhost/defaultImage.png')
+
 
         //check that the playlist is rendered
+        const displayPlaylist = screen.getByLabelText('Playlist');
+        expect(displayPlaylist).toBeInTheDocument();
+        expect(displayPlaylist).toBeVisible();
 
         //check that the date is rendered
+        const displayDate = screen.getByLabelText('Date');
+        expect(displayDate).toBeInTheDocument();
+        expect(displayDate).toBeVisible();
 
         //check that the add song button is rendered
+        const addSongsButton = screen.getByRole('button', { name: 'Add Songs' });
+        expect(addSongsButton).toBeVisible();
     });
 
     test('when add song is clicked, set mode is called with "AddPage"', () => {
