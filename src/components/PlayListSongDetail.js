cfdb5620a83cd72572f7f2943e9be90dfc6c2af9
ElPackard <42847110+ElPackard@users.noreply.github.com>
2020-11-10 20:45:54 -0500

    Update PlayListSongDetail.js

--- src/components/PlayListSongDetail.js
+++ src/components/PlayListSongDetail.js
@@ -2,10 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default function PlayListSongDetail({songDetails}) {
+   //onClick={()=> {songDetails.upvote = songDetails.upvote + 1}}
+   //onClick={()=> {songDetails.upvote = songDetails.upvote - 1}
   return (
     <div>
-      //onClick={()=> {songDetails.upvote = songDetails.upvote + 1}}
-      //onClick={()=> {songDetails.upvote = songDetails.upvote - 1}
       {songDetails.title} by {songDetails.artist}
       <button>^</button>
       <button>V</button>
