74aeea7391a607ff8f37b9fcba67370bbd06192d
hjarai <harai@middlebury.edu>
2020-11-17 17:24:29 -0500

    created knexfile, OneList.sqlite3, migrations directory with schema inside, models directory with host.js, onelist.js, song.js, src/lib directory with backend-utils.js (and test)

---
+++ migrations/20201117152107_load-onelists.js
@@ -0,0 +1,38 @@
+/* eslint-disable func-names */
+/* eslint no-unused-vars: ["error", { "args": "none" }] */
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('Song', (table) => {
+        table.increments('id');
+        table.string('title');
+        table.string('artist');
+        table.string('up');
+        table.string('down');
+        table.string('username');
+        table.string('spotify').unique();
+      })
+      
+      .createTable('Host', (table) => {
+        table.increments('id');
+        table.string('spotify');
+      })
+      
+      .createTable('OneList', (table) => {
+        table.increments('id');
+        //host ID and song ID: needs to communicate with other tables
+        table.integer('song_id');
+        table.integer('host_id');
+        table.foreign('song_id').references('Song.id').onDelete('cascade');
+        table.foreign('host_id').references('Host.id').onDelete('cascade');
+        table.string('title');
+        table.text('description').notNullable();
+        table.string('date');
+        table.string('imagesrc');
+        table.string('spotify').unique();
+      });
+
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('OneList', 'Host', 'Song');
+};
