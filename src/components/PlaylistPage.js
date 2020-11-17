1f5832f8d4cc30de6c808073898698c0fab9801d
David Gikoshvili <dgikoshvili@middlebury.edu>
2020-11-16 22:42:44 -0500

    Remove button deletes in order from top

--- src/components/PlaylistPage.js
+++ src/components/PlaylistPage.js
@@ -1,4 +1,4 @@
-/* eslint-disable no-undef, no-unused-vars */
+/* eslint-disable no-undef, no-unused-vars, no-use-before-define, prefer-const*/
 
 import PlayListSongDetail from './PlayListSongDetail';
 
@@ -9,7 +9,9 @@ import { render } from 'react-dom';
 
 
 //displays the Playlist Page, takes one parameter, the OneList to be displayed
-export default function PlaylistPage({ setMode, OneList, setSongDetails }) {
+
+export default function PlaylistPage({ setMode, OneList, setSongDetails, user}) {
+
 
     //const [songsAdded, setSongsAdded] = useState(); songs added will hold list of songs added by the user 
    
@@ -39,7 +41,7 @@ export default function PlaylistPage({ setMode, OneList, setSongDetails }) {
         // console.log("UPDATED LIST ->  ", updatedList);
 
         // currentPlaylist = [...updatedList];
-        var list = document.getElementById("Playlist");
+        let list = document.getElementById("Playlist");
             list.removeChild(list.childNodes[0]);
             
           
@@ -49,13 +51,14 @@ export default function PlaylistPage({ setMode, OneList, setSongDetails }) {
     return(
         //ADD LABELS TO EACH COMPONENT
         <div> 
-            <h6 aria-label = "Event ID">{OneList.id}</h6>
+            <h6 aria-label = "Event ID">Event ID: {OneList.id}</h6>
+            <h6> Signed in as: {user} </h6>
             <h1 aria-label = "Title">{OneList.title}</h1>
             <h2 aria-label = "Description">{OneList.description}</h2>
             <h3 aria-label = "Date">{OneList.date}</h3>
             <img src = {OneList.image_path} width="150" height="150" />
+
             <ul aria-label = "Playlist" id = "Playlist">{currentPlaylist}</ul>
-            
 
             <button onClick={() => setMode('AddPage')}>Add Songs </button>
             <button>Export</button>
@@ -68,5 +71,6 @@ export default function PlaylistPage({ setMode, OneList, setSongDetails }) {
 PlaylistPage.propTypes = {
     setMode : PropTypes.func,
     OneList : PropTypes.object.isRequired,
-    setSongDetails: PropTypes.func
+    setSongDetails: PropTypes.func, 
+    user: PropTypes.string
 }
