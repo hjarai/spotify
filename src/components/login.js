db6f42fb9d7a8113a76617c3e3021781232495dc
Cheko <fmkocheko@middlebury.edu>
2020-12-07 15:44:54 +0300

    Updated nextauth to obtain user access Token that we can use to export songs to their spotify account

--- src/components/login.js
+++ src/components/login.js
@@ -1,26 +1,38 @@
-/* 
-import { useState, useEffect } from 'react';
-import { 
-  signIn,
-  signOut,
-  useSession
-} from 'next-auth/client'
-import { useRouter } from 'next/router'; 
+
+import { signIn, signOut, useSession } from 'next-auth/client'
+
 import styles from './login.module.css'
-//import CreationPage from '../pages/CreationPage'; 
+//import { useRouter } from 'next/router'; 
+
 // The approach used in this component shows how to built a sign in and sign out
 // component that works on pages which support both client and server side
 // rendering, and avoids any flash incorrect content on initial page load.
 export default function Login () {
-  const [session, loading] = useSession();
-  const router = useRouter(); 
+
+  const [session] = useSession()
+  //const router = useRouter();
+  /*  Debugginng  
   if(session){
- router.push('./HostPage')
+    console.log(session.user);
+    console.log(session.token);
   }
+  */
   return (
+
       <div >
-       <p>
+        <p >
+          {!session && <>
+           
+            <a href={`/api/auth/signin`}
+              className = {styles.button}
+              onClick={(e) => {
+              e.preventDefault()
+              signIn() }} >
+              Sign in
+              </a>
+          </>}
           {session && <>
+
             <span className={styles.signedInText}>
               <small>Signed in as</small><br/>
               <strong>{session.user.email || session.user.name}</strong>
@@ -36,68 +48,13 @@ export default function Login () {
                 Sign out
               </a>
           </>}
-          
-          {!session && <>
-            <span  
-            className={styles.notSignedInText}>You are not signed in</span>
-            <button className="signInButton">
-            <a href={`/api/auth/signin`}
-              className = {styles.button}
-              id= "signInLink"
-              onClick={(e) => {
-              e.preventDefault()
-              signIn() }} >
-              Sign in
-              </a>
-              </button>
-          </>}
-         
-          </p>
+        </p>
       </div>
+
   )
 }
-    
-//     <div>
-//         <button className="signInButton">
-//           {!session && <>
-//             <a id= "signInLink"
-//             href={`/api/auth/signin`}
-//               className = {styles.button}
-//               onClick={(e) => {
-//               e.preventDefault()
-//               signIn() }} >
-//               Sign in
-//               </a>
-//           </>}
-//           </button>
-      
-    
-//             <p>
-//           {session && <>
 
-//             <span className={styles.signedInText}>
-//               <small>Signed in as</small><br/>
-//               <strong>{session.user.email || session.user.name}</strong>
-//               </span>
-//             <a
-//                 href={`/api/auth/signout`}
-//                 className = {styles.button}
-//                 onClick={(e) => {
-//                   e.preventDefault()
-//                   signOut()
-//                 }}
-//               >
-//                 Sign out
-//               </a>
-//           </>}
-//         </p>
-      
-//       </div>
-
-//   )
-// }
-
- */
+ /*
 
 import { useState, useEffect } from 'react';
 
@@ -161,4 +118,6 @@ export default function Login () {
 
   )
 }
+
+*/
  
--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -3,16 +3,39 @@ import Providers from 'next-auth/providers'
 
  // scopes get permission to modify user playlist ( we will use to export songs)
 
+ // The code is updated according to the new changes in nextauth to enable us get access to a user's spotify account and add
+ // a new playlist, specifically, the access Token. 
+
 const options = {
   providers: [
 
     Providers.Spotify({
-      scope: 'user-read-email playlist-modify-public playlist-modify-private',
+      scope: ' user-read-private user-read-email playlist-modify-public playlist-modify-private',
       clientId: process.env.SPOTIFY_CLIENT_ID,
       clientSecret: process.env.SPOTIFY_CLIENT_SECRET
     }),
   ],
+  session:{
+    jwt: true,
+  }, 
+  callbacks: {
+    async jwt(token, _, account) {
+      if(account){
+        token.id = account.id;
+        token.accessToken = account.accessToken;   // get access token to modify user's playlist 
+        //console.log("This is token.accessToken" +   token.accessToken );
+      }
+      return token 
+    },
+    async session(session, user) {
+      session.user = user;  // get user information i.e id, email, name and profile picture
+      //console.log("This is user" +   user); 
+     
+      return session 
+    }
+  }, 
 };
+
 
 
 
